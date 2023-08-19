e7a7605afbe349cee2fbeec48c9a0e8982429a7a
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-08-19 11:01:28 +0530

    test section completed with all the effects

--- emoquotient/src/App.js
+++ emoquotient/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import OpenVacancies from './components/OpenVacancies/OpenVacancies';
+import Test from './components/Test/Test';
+import WorkWithUsSection from './components/Work/WorkWithUs';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Test/>
+      <WorkWithUsSection/>
       <OpenVacancies/>
       <Footer/>
     </div>
